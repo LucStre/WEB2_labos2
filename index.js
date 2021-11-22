ee39737318474afbf5a5520b41645a1d5c73d53a
Lucija <lucija.strejcek@fer.hr>
2021-11-22 19:17:36 +0100

    Add bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
