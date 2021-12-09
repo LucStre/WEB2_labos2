d6ee08721f2ac457bfdc16c069be540003ce1a78
Lucija <lucija.strejcek@fer.hr>
2021-12-09 19:56:36 +0100

    Add user login and signup

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const path = require("path");
+const port = 3000;
+
+const homeRouter = require("./routes/home");
+const signupRouter = require("./routes/signup");
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+
+const userData = require("./model/UserData");
+
+//express middleware setup
+const app = express();
+
+//cookie-parsing middleware
+app.use(cookieParser());
+
+//body content parsing middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//session middleware (transient session records)
+app.use(session({ secret: "WEB2", resave: false, saveUninitialized: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
+
+app.set("views", path.join(__dirname, "views"));
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// routes
+app.use("/", homeRouter);
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
+
+//read the user settings
+app.users = new userData("./data/users.json");
+app.users.initialize(true);
+
+// use res.render to load up an ejs view file
+// home page
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ routes/login.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+const cookieParser = require("cookie-parser");
+
+router.get("/", function (req, res, next) {
+  let loginName = req.cookies.appuserID || "";
+  res.render("login", {
+    err: undefined,
+    user: req.session.user,
+    login: loginName,
+  });
+});
+
+router.post("/", function (req, res, next) {
+  //check that the user is registered
+  if (req.session.user !== undefined) {
+    res.render("login", {
+      user: req.session.user,
+      err: "Please log out first.",
+    });
+    return;
+  }
+
+  //check for credentials
+  if (
+    req.app.users.userExists(req.body.username) &&
+    req.app.users.getUser(req.body.username).password == req.body.password
+  ) {
+    //if successful, set persistent cookie with username (timeout=1 week)
+    let expiryDate = new Date(Number(new Date()) + 604800000);
+    res.cookie("appuserID", req.body.username, {
+      expires: expiryDate,
+      httpOnly: true,
+    });
+
+    //if successful, redirect to the main page
+    req.session.user = req.body.username;
+
+    res.render("home", { user: req.body.username });
+  } else {
+    res.render("login", {
+      user: req.session.user,
+      err: "Incorrect username or password.",
+    });
+  }
+});
+
+module.exports = router;
