a9b2d5ee84368a53008df902154fbe1d15b8869c
Lucija <lucija.strejcek@fer.hr>
2021-12-12 16:38:01 +0100

    Add admin access

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,25 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  //check if a registered user is trying to access the admin page
-  if (req.session.user === undefined) {
-    res.render("admin", { user: req.session.user, err: "Forbidden access." });
-    return;
-  }
+  if (res.app.get("secure")) {
+    if (req.session.user === undefined) {
+      //check if a registered user is trying to access the admin page
+      res.render("admin", {
+        user: req.session.user,
+        err: "Forbidden access.",
+      });
+      return;
+    }
 
-  //check for the 'admin' role of the user trying to access
-  if (req.app.users.getUser(req.session.user).role != "admin") {
-    res.render("admin", { user: req.session.user, err: "Forbidden access." });
-    return;
+    //check for the 'admin' role of the user trying to access
+    if (req.app.users.getUser(req.session.user).role != "admin") {
+      res.render("admin", { user: req.session.user, err: "Forbidden access." });
+      return;
+    }
   }
 
   //render the page
-  res.render("admin", { user: req.session.user, err: undefined });
+  res.render("admin", { user: req.session.user, err: undefined, secure: true });
 });
 
 module.exports = router;
