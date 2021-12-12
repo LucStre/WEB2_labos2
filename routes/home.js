12217d09217724435a2403ec1caa52b75d229432
Lucija <lucija.strejcek@fer.hr>
2021-12-12 16:37:21 +0100

    Fix security switch

--- routes/home.js
+++ routes/home.js
@@ -4,14 +4,17 @@ const session = require("express-session");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.app.set("secure", true);
   res.render("home", { user: req.session.user, secure: true });
 });
 
 router.post("/", function (req, res, next) {
-  if (req.body.security == "0") {
-    res.render("home", { user: req.session.user, secure: true });
-  } else if (req.body.security == "1") {
+  if (res.app.get("secure")) {
+    res.app.set("secure", false);
     res.render("home", { user: req.session.user, secure: false });
+  } else {
+    res.app.set("secure", true);
+    res.render("home", { user: req.session.user, secure: true });
   }
 });
 
